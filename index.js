ac84dd5a672084218a99c968afa89654162e4f08
azizivahid <azizi.vahid@yahoo.com>
2023-04-24 17:35:14 -0400

    complete

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+let totalBatteries = 0 
 // Code your solution here
+function totalB (strArray) {
+    
+    for (const el of strArray){
+         totalBatteries += el
+    }
+    console.log( totalBatteries)
+}
+
+totalB (batteryBatches)
